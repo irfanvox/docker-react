e3b97297ee4557c044243c92fc1152a9c85a1c44
99003728 <mohammed.ijaz@ltts.com>
2022-02-17 17:07:11 +0530

    intial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learned a lot
+          Learn React
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Learned a lot/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Learned a lot/i);
   expect(linkElement).toBeInTheDocument();
 });
